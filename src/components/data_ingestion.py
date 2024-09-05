82585dd18c9c8ae0a6af832d27338bbfe214792d
elderflamevandle <chaianyabagul07@gmail.com>
2024-09-05 17:16:16 +0530

    Prediction Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,12 +53,12 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
         
-if __name__=="__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_array, test_array,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+# if __name__=="__main__":
+#     obj = DataIngestion()
+#     train_data, test_data = obj.initiate_data_ingestion()
+#     data_transformation = DataTransformation()
+#     train_array, test_array,_ = data_transformation.initiate_data_transformation(train_data, test_data)
 
-    modeltrainer = ModelTrianer()
-    score = modeltrainer.initiate_model_trainer(train_array, test_array)
-    print(f"R2 Score:", score) 
+#     modeltrainer = ModelTrianer()
+#     score = modeltrainer.initiate_model_trainer(train_array, test_array)
+#     print(f"R2 Score:", score) 
