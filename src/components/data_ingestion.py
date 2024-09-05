5f0d38874de26641b593d27167ea0dae756627bb
elderflamevandle <chaianyabagul07@gmail.com>
2024-09-05 16:44:01 +0530

    model_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig 
 
+from src.components.model_trainer import ModelTrianer, ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     """
@@ -55,4 +57,8 @@ if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_array, test_array,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrianer()
+    score = modeltrainer.initiate_model_trainer(train_array, test_array)
+    print(f"R2 Score:", score) 
